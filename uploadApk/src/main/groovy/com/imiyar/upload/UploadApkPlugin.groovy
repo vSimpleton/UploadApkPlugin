e4717510d318bc4aa0b12e99e644d4d7be192a42
vSimpleton <masm@luxuewen.com>
2022-02-25 16:34:42 +0800

    First Commit

---
+++ uploadApk/src/main/groovy/com/imiyar/upload/UploadApkPlugin.groovy
@@ -0,0 +1,13 @@
+package com.imiyar.upload
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class UploadApkPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+
+}
